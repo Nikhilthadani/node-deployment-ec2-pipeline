d64a57cbd9169346db7ff22abd91a1f0a6b79560
Nikhil Thadani <nikhilthadani112256@gmail.com>
2025-04-19 15:21:06 +0530

    added actions change

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.get("/health", (req, res) => {
   return res.status(200).json({ message: "Health OK" });
 });
 app.get("/", (req, res) => {
-  return res.status(200).send(`<h1>Welcome to the deployed app!!!</h1`);
+  return res
+    .status(200)
+    .send(`<h1>Welcome to the deployed app!!! With ACTIONS!!!</h1`);
 });
 app.listen(80, () => console.log("Server Running"));
